dbabdff8e3e4fa8180e2dae6781bffc3bf2d212c
Gnanesh Kotyada <gnaneshkotyada410@gmail.com>
2025-12-15 08:21:44 +0530

    fixed Module not found issue

--- frontend/src/pages/ProductWrapper.jsx
+++ frontend/src/pages/ProductWrapper.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import Product from "./pages/Product";
+import Product from "./Product";
 
 const ProductWrapper =()=> {
   const { productId } = useParams();
