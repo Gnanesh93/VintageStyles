392dfa6b15692fcc15999279ba101a96f8d29cd9
Gnanesh Kotyada <gnaneshkotyada410@gmail.com>
2026-02-01 09:33:47 +0530

    Fixed user Not found issue while submitting reviews

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,19 +1,26 @@
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 
-const authUser= async(req,res,next)=>{
-    const{token}=req.headers;
-    if(!token){
-        return res.json({success:false,message:'Not Authorized! Login again'})
-    }
-    try {
-       const token_decode=jwt.verify(token,process.env.JWT_SECRET)
-       req.body.userId=token_decode.id
-       next()
-    } 
-    catch (error) {
-      console.log(error) 
-      res.json({success:false,message:error.message}) 
-    }
-}
+const authUser = async(req,res,next)=>{
+  const {token} = req.headers;
+
+  if (!token){
+    return res.json({success:false, message: "Not Authorized! Login again"});
+  }
+
+  try{
+    const decoded = jwt.verify(token,process.env.JWT_SECRET);
+    req.body.userId = decoded.id;
+
+    //only for reviews
+    req.userId = decoded.id;
+    req.userName = decoded.name || "Customer";
+
+    next();
+  } 
+  catch (error){
+    console.log(error);
+    res.json({success:false, message:error.message});
+  }
+};
 
 export default authUser;
