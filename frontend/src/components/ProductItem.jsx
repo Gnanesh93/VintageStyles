7483b4bb9ecc05ce3cb062b5ba7f6788e65d9953
Gnanesh <gnaneshkotyada410@gmail.com>
2026-06-02 16:42:27 +0530

    fix: product id undefined

--- frontend/src/components/ProductItem.jsx
+++ frontend/src/components/ProductItem.jsx
@@ -2,11 +2,11 @@ import {useContext} from 'react'
 import { ShopContext } from '../context/ShopContext'
 import { Link } from 'react-router-dom';
 
-const ProductItem = ({id,image,name,price}) => {
+const ProductItem = ({_id,image,name,price}) => {
     const {currency} = useContext(ShopContext);
 
     return (
-        <Link className="text-gray-700 cursor-pointer" to={`/product/${id}`}>
+        <Link className="text-gray-700 cursor-pointer" to={`/product/${_id}`}>
             <div className="w-full h-48 overflow-hidden rounded-md bg-gray-100 flex justify-center items-center">
                 <img src={image[0]} alt={name} className="w-full h-full object-cover hover:scale-110 transition ease-in-out" />
             </div>
